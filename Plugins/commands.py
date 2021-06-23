d736ecebc32810f998a546b939fc03bf13cacb67
BHASHITHA <Pramudith.bhashitha17@gmail.com>
2021-06-23 16:30:30 +0530

    Add files via upload

---
+++ Plugins/commands.py
@@ -0,0 +1,139 @@
+#MIT License
+
+#Copyright (c) 2021 subinps
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, filters
+from config import Config
+import asyncio
+import sys
+import os
+
+USER=Config.USER
+OWNER=Config.OWNER
+HOME_TEXT=Config.HOME_TEXT
+HOME_TEXT_OWNER=Config.HOME_TEXT_OWNER
+HELP=Config.HELP
+
+
+@Client.on_message(filters.command("start") & filters.private)
+async def start(bot, cmd):
+	if str(cmd.from_user.id) != OWNER:	
+		await cmd.reply_text(
+			HOME_TEXT.format(cmd.from_user.first_name, cmd.from_user.id, USER, USER, USER, OWNER), 
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(
+				[
+					[
+						InlineKeyboardButton("⚡ Group ⚡", url='https://t.me/NET_HACKER_BOTs_chat'),
+						InlineKeyboardButton("🤖Other Bots", url="https://t.me/NET_HACKER_BOTs/46")
+					],
+                    [
+                        InlineKeyboardButton("🔗Source Code", url="https://github.com/NET-HACKER-BOTs/Instagram-bot"),
+						InlineKeyboardButton("🧩Deploy Own Bot", url="https://heroku.com/deploy?templat=https://github.com/NET-HACKER-BOTs/Instagram-bot")
+                    ],
+                    [
+                        InlineKeyboardButton("👨🏼‍🦯How To Use?", callback_data="help#subin"),
+						InlineKeyboardButton("⚙️Update Channel", url="https://t.me/NET_HACKER_BOTs")
+
+                    ]
+					
+				]
+			)
+		)
+	else:
+		await cmd.reply_text(
+			HOME_TEXT_OWNER.format(cmd.from_user.first_name, cmd.from_user.id), 
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(
+				[
+					[
+						InlineKeyboardButton("⚡ Group ⚡", url='https://t.me/NET_HACKER_BOTs_chat'),
+						InlineKeyboardButton("🤖Other Bots", url="https://t.me/NET_HACKER_BOTs/46"),
+					],
+                    [
+                        InlineKeyboardButton("🔗Source Code", url="https://github.com/NET-HACKER-BOTs/Instagram-bot")
+                    ],
+                    [
+                        InlineKeyboardButton("👨🏼‍🦯How To Use?", callback_data="help#subin"),
+						InlineKeyboardButton("⚙️Update Channel", url="https://t.me/NET_HACKER_BOTs")
+
+                    ]
+					
+				]
+			)
+		)
+
+
+@Client.on_message(filters.command("help") & filters.private)
+async def help(bot, cmd):
+	await cmd.reply_text(
+		HELP,
+		disable_web_page_preview=True,
+		reply_markup=InlineKeyboardMarkup(
+			[
+				[
+					InlineKeyboardButton("⚡ Group ⚡", url='https://t.me/NET_HACKER_BOTs_chat'),
+					InlineKeyboardButton("🤖Other Bots", url="https://t.me/NET_HACKER_BOTs/46"),
+					InlineKeyboardButton("⚙️Update Channel", url="https://t.me/NET_HACKER_BOTs")
+					
+				],
+				[
+					InlineKeyboardButton("🔗Source Code", url="https://github.com/NET-HACKER-BOTs/Instagram-bot"),
+					InlineKeyboardButton("🧩Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/NET-HACKER-BOTs/Instagram-bot")
+				]
+			]
+			)
+		)
+
+@Client.on_message(filters.command("restart") & filters.private)
+async def stop(bot, cmd):
+	if str(cmd.from_user.id) != OWNER:	
+		await cmd.reply_text(
+			HOME_TEXT.format(cmd.from_user.first_name, cmd.from_user.id, USER, USER, USER, OWNER), 
+			disable_web_page_preview=True,
+			reply_markup=InlineKeyboardMarkup(
+				[
+					[
+						InlineKeyboardButton("⚡ Group ⚡", url='https://t.me/NET_HACKER_BOTs_chat'),
+						InlineKeyboardButton("🤖Other Bots", url="https://t.me/NET_HACKER_BOTs/46")	
+					],
+                    [
+                        InlineKeyboardButton("🔗Source Code", url="https://github.com/NET-HACKER-BOTs/Instagram-bot"),
+						InlineKeyboardButton("🧩Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/NET-HACKER-BOTs/Instagram-bot")
+                    ],
+                    [
+                        InlineKeyboardButton("👨🏼‍🦯How To Use?", callback_data="help#subin"),
+						InlineKeyboardButton("⚙️Update Channel", url="https://t.me/NET_HACKER_BOTs")
+
+                    ]
+					
+				]
+			)
+		)
+		return
+	msg = await bot.send_message(
+		text="Restarting your bot..",
+		chat_id=cmd.from_user.id
+		)
+	await asyncio.sleep(2)
+	await msg.edit("All Processes Stopped and Restarted")
+	os.execl(sys.executable, sys.executable, *sys.argv)
